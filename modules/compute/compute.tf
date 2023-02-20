b2ffb6a85e13a20f77e0ad6e72a0e538706076d5
Fausto Soares <41522884+faustosm@users.noreply.github.com>
2023-02-20 20:04:42 -0300

    Update compute.tf

--- modules/compute/compute.tf
+++ modules/compute/compute.tf
@@ -50,7 +50,7 @@ EOF
 
 
 resource "aws_instance" "test_ec2" {
-    ami           = "ami-005e54dee72cc1d00" # us-west-2
+    ami           = "ami-0dfcb1ef8550277af"
     instance_type = "t2.micro"
     subnet_id = var.subnet_id  
 }

36c097a314c76dc2e143dd7f1c23e494e72ad977
Fausto Soares <41522884+faustosm@users.noreply.github.com>
2023-02-20 19:37:11 -0300

    Update networking.tf

--- modules/networking/networking.tf
+++ modules/networking/networking.tf
@@ -1,16 +1,3 @@
-resource "aws_vpc" "aws_vpc_example" {
-    cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "example-vpc"
-  }
-}
-
-resource "aws_subnet" "aws_subnet_example" {
-  cidr_block = "10.0.0.0/16"
-  vpc_id     = aws_vpc.aws_vpc_example.id
-  
-  tags = {
-    Name = "aws_subnet_example"
-  }
+output "id" {
+  value = aws_subnet.aws_subnet_example.id
 }

eedb88511afa661cb948dff43c6f33f53bdc89b8
Fausto <faustosoares.m@gmail.com>
2023-02-23 19:43:33 -0300

    add

--- modules/notifications/sns.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sns_topic" "user_updates" {
-  name = "user-updates-topic"
-}
-
-resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-  topic_arn = aws_sns_topic.user_updates.arn
-  protocol  = "email"
-  endpoint  = "faustosoares.m@gmail.com"
-}
